19ca26308ca211a1a5c16bdfec9f618c879d5d6f
ashir876 <iamjunaid021@gmail.com>
2024-09-26 15:16:10 +0500

    ADDED: chadCN and layout addded

---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header/Header";
+import ReservationForm from "@/components/ReservationForm";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Header />
+      <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <ReservationForm />
+      </main>
+    </div>
+  );
+}
