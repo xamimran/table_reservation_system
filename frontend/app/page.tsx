f80e6e1741cdbdfa5d3eb2e1da85d71bf66de059
ashir876 <iamjunaid021@gmail.com>
2024-10-07 19:56:59 +0500

    ADDED: react-int8 package added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,32 @@
-'use client'
+"use client";
 import ReservationForm from "@/components/ReservationForm";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import Image from "next/image";
 import { PUBLISH_KEY } from "./constantVariable/constant";
+import { LanguageProvider } from "./contexts/LanguageContext";
+import LanguageSwitcher from "@/components/LanguageSwitcher";
 const stripePromise = loadStripe(PUBLISH_KEY);
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100">
       <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="flex justify-center mb-4">
-          <Image
-            src="/client-logo.png"
-            width={300}
-            height={250}
-            alt="Restaurant Logo"
-          />
-        </div>
-        <Elements stripe={stripePromise}>
-          <ReservationForm />
-        </Elements>{" "}
+        <LanguageProvider>
+          <div className="fixed top-4 right-4 z-50">
+            <LanguageSwitcher />
+          </div>
+          <div className="flex justify-center mb-4">
+            <Image
+              src="/client-logo.png"
+              width={300}
+              height={250}
+              alt="Restaurant Logo"
+            />
+          </div>
+          <Elements stripe={stripePromise}>
+            <ReservationForm />
+          </Elements>{" "}
+        </LanguageProvider>
       </main>
     </div>
   );
