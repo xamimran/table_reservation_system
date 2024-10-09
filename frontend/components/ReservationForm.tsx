b1ebb9027f7be429173b85d8b9358dfd22397b2f
ashir876 <iamjunaid021@gmail.com>
2024-10-09 13:03:31 +0500

    ADDED: handle card saved functionality added

--- frontend/components/ReservationForm.tsx
+++ frontend/components/ReservationForm.tsx
@@ -13,7 +13,7 @@ import { useToast } from "@/hooks/use-toast";
 import DefaultLoader from "./DefaultLoader";
 import Image from "next/image";
 import { useTranslations } from "@/app/hooks/useTranslations";
-import { useLanguage } from "@/app/contexts/LanguageContext";
+import CheckoutForm from "./Stripe/CheckoutForm";
 
 const steps = [
   { title: "mealType", icon: "/meal-type.png", component: MealTypeStep },
@@ -21,7 +21,6 @@ const steps = [
   { title: "date", icon: "/date.png", component: DateStep },
   { title: "details", icon: "/details.png", component: CustomerDetailsStep },
 ];
-
 export default function ReservationForm() {
   const t = useTranslations();
   const [currentStep, setCurrentStep] = useState(0);
@@ -42,6 +41,11 @@ export default function ReservationForm() {
   const [availabilityChecked, setAvailabilityChecked] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const { toast } = useToast();
+  const [clientSecret, setClientSecret] = useState(null);
+  const [customerId, setCustomerId] = useState(null);
+  const [paymentId, setPaymentId] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [reservationId, setReservationId] = useState(null);
 
   const handleStepClick = (index: number) => {
     if (index < currentStep) {
@@ -75,7 +79,12 @@ export default function ReservationForm() {
           customerDetails,
           tableData,
         });
-        window.location.href = response.data.paymentUrl;
+        setClientSecret(response.data.clientSecret);
+        setCustomerId(response.data.customerId);
+        setPaymentId(response.data.paymentId);
+        setUserId(response.data.userId);
+        setReservationId(response.data.reservationId);
+        // window.location.href = response.data.paymentUrl;
       } catch (error: any) {
         let errorMsg = error.response?.data?.error || "An error occurred";
         toast({
@@ -170,128 +179,173 @@ export default function ReservationForm() {
         return null;
     }
   };
+  const handleCardSaved = async (
+    paymentMethodId: string,
+    customerId: string
+  ) => {
 
+    try {
+      // Call the get-session API
+      const response = await fetch(
+        `/api/get-session?payment_method_id=${paymentMethodId}&customer_id=${customerId}&payment_id=${paymentId}&user_id=${userId}&reservation_id=${reservationId}`,
+        {
+          method: "GET",
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to save card");
+      }
+
+      const data = await response.json();
+
+      // Handle the response based on the card save status
+      if (data.applicationStatus === "PA") {
+        console.log("Card saved successfully");
+        // You can add any additional client-side logic here for successful card save
+      } else if (data.applicationStatus === "F") {
+        console.log("Failed to save card");
+        // You can add any additional client-side logic here for failed card save
+      }
+
+      // Redirect to the appropriate page
+      window.location.href = data.applicationLink;
+    } catch (error) {
+      console.error("Error saving card:", error);
+      // Handle the error (e.g., show an error message to the user)
+    }
+  };
   return (
-    <div className="max-w-4xl mx-auto mt-4 sm:mt-10 p-4 sm:p-8 bg-white rounded-lg shadow-lg">
-      <h1 className="text-2xl sm:text-3xl font-bold mb-4 sm:mb-8 text-center text-gray-800">
-        {t("tableReservation")}
-      </h1>
-      <div className="mb-4 sm:mb-8">
-        <ol className="flex flex-wrap sm:flex-nowrap items-center w-full p-2 sm:p-3 space-x-1 sm:space-x-2 justify-between text-xs sm:text-sm font-medium text-center text-gray-500 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 sm:text-base dark:bg-gray-800 dark:border-gray-700">
-          {steps.map((step, index) => (
-            <li
-              key={step.title}
-              className={cn(
-                "flex items-center cursor-pointer mb-2 sm:mb-0",
-                index <= currentStep ? "text-[#eab24f]" : "text-gray-500"
-              )}
-              onClick={() => handleStepClick(index)}
-            >
-              <span
-                className={cn(
-                  "flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 mr-1 sm:mr-2 rounded-full text-xs shrink-0",
-                  index < currentStep
-                    ? "border-[#eab24f] bg-[#eab24f] text-white"
-                    : "border-gray-500"
-                )}
-              >
-                {index < currentStep ? (
-                  "✓"
-                ) : (
-                  <Image
-                    src={step.icon}
-                    alt={step.icon}
-                    width="32"
-                    height="32"
-                    className="w-4 h-4 sm:w-6 sm:h-6"
-                  />
-                )}
-              </span>
-              <span className="hidden sm:inline">{t(step.title)}</span>
-              {index < steps.length - 1 && (
-                <svg
-                  aria-hidden="true"
-                  className="w-3 h-3 ml-1 sm:w-4 sm:h-4 sm:ml-2"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
+    <>
+      {clientSecret && customerId ? (
+        <CheckoutForm
+          onSuccess={handleCardSaved}
+          clientSecret={clientSecret}
+          customerId={customerId}
+        />
+      ) : (
+        <div className="max-w-4xl mx-auto mt-4 sm:mt-10 p-4 sm:p-8 bg-white rounded-lg shadow-lg">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-4 sm:mb-8 text-center text-gray-800">
+            {t("tableReservation")}
+          </h1>
+          <div className="mb-4 sm:mb-8">
+            <ol className="flex flex-wrap sm:flex-nowrap items-center w-full p-2 sm:p-3 space-x-1 sm:space-x-2 justify-between text-xs sm:text-sm font-medium text-center text-gray-500 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 sm:text-base dark:bg-gray-800 dark:border-gray-700">
+              {steps.map((step, index) => (
+                <li
+                  key={step.title}
+                  className={cn(
+                    "flex items-center cursor-pointer mb-2 sm:mb-0",
+                    index <= currentStep ? "text-[#eab24f]" : "text-gray-500"
+                  )}
+                  onClick={() => handleStepClick(index)}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M13 5l7 7-7 7M5 5l7 7-7 7"
-                  ></path>
-                </svg>
-              )}
-            </li>
-          ))}
-        </ol>
-      </div>
-      <form>
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={currentStep}
-            initial={{ opacity: 0, x: 50 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -50 }}
-            transition={{ duration: 0.3 }}
-          >
-            {renderStep()}
-          </motion.div>
-        </AnimatePresence>
-        <div className="mt-4 sm:mt-8 flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0">
-          {currentStep > 0 ? (
-            <Button
-              type="button"
-              variant="outline"
-              onClick={handlePrevious}
-              className="text-base sm:text-lg px-4 sm:px-6 py-2 sm:py-3 w-full sm:w-auto"
-            >
-              {t("previous")}
-            </Button>
-          ) : (
-            <div className="hidden sm:block"></div>
-          )}
-          <div className="flex flex-col sm:flex-row justify-end space-y-4 sm:space-y-0 sm:space-x-4 w-full sm:w-auto">
-            {currentStep === 2 && (
-              <Button
-                type="button"
-                onClick={handleAvailability}
-                className="text-base sm:text-lg px-4 sm:px-6 py-2 sm:py-3 w-full sm:w-auto"
-              >
-                {t("checkAvailability")}{" "}
-              </Button>
-            )}
-            {currentStep < steps.length - 1 ? (
-              <Button
-                type="button"
-                onClick={handleNext}
-                disabled={
-                  (currentStep === 0 && !mealType) ||
-                  (currentStep === 2 &&
-                    !isTableAvailable &&
-                    !availabilityChecked)
-                }
-                className="text-base sm:text-lg px-4 sm:px-6 py-2 sm:py-3 w-full sm:w-auto"
-              >
-                {t("next")}
-              </Button>
-            ) : (
-              <Button
-                type="button"
-                onClick={handleSubmit}
-                className="text-base sm:text-lg px-4 sm:px-6 py-2 sm:py-3 w-full sm:w-auto"
-              >
-                {t("submitReservation")}
-                {/* Submit Reservation */}
-              </Button>
-            )}
+                  <span
+                    className={cn(
+                      "flex items-center justify-center w-6 h-6 sm:w-8 sm:h-8 mr-1 sm:mr-2 rounded-full text-xs shrink-0",
+                      index < currentStep
+                        ? "border-[#eab24f] bg-[#eab24f] text-white"
+                        : "border-gray-500"
+                    )}
+                  >
+                    {index < currentStep ? (
+                      "✓"
+                    ) : (
+                      <Image
+                        src={step.icon}
+                        alt={step.icon}
+                        width="32"
+                        height="32"
+                        className="w-4 h-4 sm:w-6 sm:h-6"
+                      />
+                    )}
+                  </span>
+                  <span className="hidden sm:inline">{t(step.title)}</span>
+                  {index < steps.length - 1 && (
+                    <svg
+                      aria-hidden="true"
+                      className="w-3 h-3 ml-1 sm:w-4 sm:h-4 sm:ml-2"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13 5l7 7-7 7M5 5l7 7-7 7"
+                      ></path>
+                    </svg>
+                  )}
+                </li>
+              ))}
+            </ol>
           </div>
+          <form>
+            <AnimatePresence mode="wait">
+              <motion.div
+                key={currentStep}
+                initial={{ opacity: 0, x: 50 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: -50 }}
+                transition={{ duration: 0.3 }}
+              >
+                {renderStep()}
+              </motion.div>
+            </AnimatePresence>
+            <div className="mt-4 sm:mt-8 flex flex-col sm:flex-row justify-between items-center space-y-4 sm:space-y-0">
+              {currentStep > 0 ? (
+                <Button
+                  type="button"
+                  variant="outline"
+                  onClick={handlePrevious}
+                  className="text-base sm:text-lg px-4 sm:px-6 py-2 sm:py-3 w-full sm:w-auto"
+                >
+                  {t("previous")}
+                </Button>
+              ) : (
+                <div className="hidden sm:block"></div>
+              )}
+              <div className="flex flex-col sm:flex-row justify-end space-y-4 sm:space-y-0 sm:space-x-4 w-full sm:w-auto">
+                {currentStep === 2 && (
+                  <Button
+                    type="button"
+                    onClick={handleAvailability}
+                    className="text-base sm:text-lg px-4 sm:px-6 py-2 sm:py-3 w-full sm:w-auto"
+                  >
+                    {t("checkAvailability")}{" "}
+                  </Button>
+                )}
+                {currentStep < steps.length - 1 ? (
+                  <Button
+                    type="button"
+                    onClick={handleNext}
+                    disabled={
+                      (currentStep === 0 && !mealType) ||
+                      (currentStep === 2 &&
+                        !isTableAvailable &&
+                        !availabilityChecked)
+                    }
+                    className="text-base sm:text-lg px-4 sm:px-6 py-2 sm:py-3 w-full sm:w-auto"
+                  >
+                    {t("next")}
+                  </Button>
+                ) : (
+                  <Button
+                    type="button"
+                    onClick={handleSubmit}
+                    className="text-base sm:text-lg px-4 sm:px-6 py-2 sm:py-3 w-full sm:w-auto"
+                  >
+                    {t("submitReservation")}
+                    {/* Submit Reservation */}
+                  </Button>
+                )}
+              </div>
+            </div>
+          </form>
+          {isLoading && <DefaultLoader />}
         </div>
-      </form>
-      {isLoading && <DefaultLoader />}
-    </div>
+      )}
+    </>
   );
 }
